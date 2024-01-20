478d6497a830e0cdd31a04b38b5fbf52a52e777e
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-01-20 15:21:52 -0600

    Added Header component with styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,10 @@
 import Image from "next/image";
-import Link from "next/link";
+import Header from "./components/layout/Header";
 
 export default function Home() {
   return (
     <>
-    <header>
-      <Link href=""> ST Hamburger</Link>
-      <nav>
-        <Link href={''}>Home</Link>
-        <Link href={''}>Menu</Link>
-        <Link href={''}>About</Link>
-        <Link href={''}>Contact</Link>
-        <Link href={''}>Login</Link>
-      </nav>
-    </header>
+    <Header />
     </>
   );
 }

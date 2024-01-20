bd1b901a49aaec2fdb93dbacacf684db1350ada7
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-01-20 15:03:35 -0600

    Added Header with navigation links

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (<div>Testing</div>
+  return (
+    <>
+    <header>
+      <Link href=""> ST Hamburger</Link>
+      <nav>
+        <Link href={''}>Home</Link>
+        <Link href={''}>Menu</Link>
+        <Link href={''}>About</Link>
+        <Link href={''}>Contact</Link>
+        <Link href={''}>Login</Link>
+      </nav>
+    </header>
+    </>
   );
 }

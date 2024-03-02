5ddfe3e8dd179a9c38345a25e85708cad4cbddb1
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-02 11:13:37 -0700

    Added menu and checkout section

---
+++ src/app/components/layout/HomeMenu.js
@@ -0,0 +1,15 @@
+export default function HomeMenu() {
+ return (
+    <section className="">
+        <div className="text-center">
+            <h3 className="uppercase text-gray-500 font-semibold
+            leading-4">
+                Check out
+            </h3>
+            <h2 className="text-primary font-bold text-4xl italic">
+                Menu
+            </h2>
+        </div>
+    </section>
+ )
+}

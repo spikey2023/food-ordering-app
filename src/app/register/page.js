6a6ba9af489926306899d56b2544ddd15e731116
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-02 14:31:33 -0700

    Added Log in with Google button

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,21 @@
+import Image from "next/image"
 export default function RegistePage() {
     return(
-        <div>Register</div>
+        <section className="mt-8">
+            <h1 className="text-center text-primary text-4xl mb-4">
+                Register
+            </h1>
+            <form className="block max-w-xs mx-auto">
+                <input type="email" placeholder="email"></input>
+                <input type="password" placeholder="password"></input>
+                <button type="submit">Register</button>
+                <div className="my-4 text-center text-gray-500">
+                    or login with provider </div>
+                <button className="flex gap-4 justify-center">
+                    <Image  src={'/google.png'} width={24} height={24}
+                    alt={'log in with google'}/>
+                    Login with Google</button>
+            </form>
+        </section>
     )
 }

975e48d0440b607ea713271c01246c871fbf8bad
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-02 12:05:20 -0700

    Added styling for menu and checkout button

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -1,6 +1,20 @@
+import Image from "next/image"
+
+
 export default function HomeMenu() {
  return (
     <section className="">
+      <div className="absolute h-64 left-0 right-0 w-full
+      justify-start">
+        <div className="h-48 w-48 absolute -left-20 -top-20 -z-10">
+            <Image src={'/salad1.png'} layout={'fill'}
+            objectFit={'contain'} alt={'salad'} />
+        </div>
+        <div className="h-48 w-48 absolute -top-24 -right-20 -z-10">
+            <Image src={'/salad2.png'} layout={'fill'}
+            objectFit={'contain'} alt={'salad'} />
+            </div>
+        </div>
         <div className="text-center">
             <h3 className="uppercase text-gray-500 font-semibold
             leading-4">

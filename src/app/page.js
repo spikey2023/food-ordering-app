ac9f53cc5bd07a75937d6bb8e59206e105416523
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-02 13:03:46 -0700

    Added footer section

--- src/app/page.js
+++ src/app/page.js
@@ -51,6 +51,9 @@ export default function Home() {
         </a>
       </div>
       </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+          &copy; 2024 All rights reserved
+      </footer>
     </>
   );
 }

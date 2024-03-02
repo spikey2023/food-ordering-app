1b22621274ecbb22ed04094f177c092b02fde5e4
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-02 12:54:44 -0700

    Added About us section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,45 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
     <Header />
     <Hero />
-    <HomeMenu/>
+    <HomeMenu />
+    <section className="text-center my-16">
+      <SectionHeaders 
+        subHeader={'Our Story'}
+        mainHeader={'About us'}
+        />
+        <div className="text-gray-500 max-w-2xl mx-auto mt-4 flex
+        flex-col gap-4">
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+         in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+         nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+         in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+         nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        </div>
+    </section>
     </>
   );
 }

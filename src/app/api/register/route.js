035328900783ba2f564ce7672227fa2aa837a835
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-09-09 11:00:20 -0700

    "Successful Database Connection"

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,10 +1,11 @@
-import { User } from "@/app/models/User"
+import {User} from "@/app/models/User"
 import mongoose from "mongoose"
+
 
 export async function POST(req) {
     const body = await req.json();
     //connect to database
-    mongoose.connect(process.env.MONGO_URL)
+   mongoose.connect(process.env.MONGO_URL)
     //create user
     const createdUser = await User.create(body)
     return Response.json(createdUser)

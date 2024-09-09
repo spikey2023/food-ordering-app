6b582de2cb3c999fa2e9e3b63f608af1c37b5d96
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-09-09 11:59:47 -0700

    "Register styling and error updated"

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,27 @@
 import {model, models, Schema} from "mongoose";
+import bcrypt from 'bcrypt'
 
 const UserSchema = new Schema({
   name: {type: String},
   email: {type: String, required: true, unique: true},
-  password: {type: String},
+  password: {
+    type: String, 
+    required : true,
+    validate: pass => {
+      if (!pass?.length || pass.length <5 ) {
+        new Error('password must be at least 5 characters long')
+      }
+    }  
+  },
   image: {type: String},
 }, {timestamps: true});
+
+//Encrypt the password after validation using Bcrypt
+UserSchema.post('validate', function (user) {
+  const notHashedPassword = user.password
+  const salt = bcrypt.genSaltSync(10)
+  user.password = bcrypt.hashSync(notHashedPassword, salt)
+  
+})
 
 export const User = models?.User || model('User', UserSchema);
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,21 +1,35 @@
 "use client";
 
 import Image from "next/image"
+import Link from "next/link";
 import {useState} from "react"
 
 export default function RegisterPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [creatingUser, setCreatingUser] = useState(false)
+    const [userCreated, setUserCreated] = useState(false)
+    const [error, setError] = useState(false)
     
     async function handleFormSubmit(ev){
         ev.preventDefault()
+        setCreatingUser(true)
+        setError(false)
+        setUserCreated(false)
         //send a requeust to the register route inside the api directory
-        const response = await fetch('/api/register', {
-            method: 'POST',
-            body: JSON.stringify({email, password}),
-            headers: {'Content-Type': 'application/json'},     
-        } )
-
+            const response = await fetch('/api/register', {
+                method: 'POST',
+                body: JSON.stringify({email, password}),
+                headers: {'Content-Type': 'application/json'},     
+            } )
+            if (response.ok) {
+                setUserCreated(true) 
+            }else {
+                setError(true)
+            }
+            setCreatingUser(false) 
+            
+        
     }
 
     return(
@@ -23,12 +37,29 @@ export default function RegisterPage() {
             <h1 className="text-center text-primary text-4xl mb-4">
                 Register
             </h1>
+            {userCreated && (
+                <div className="my-4 text-center" >
+                    User Created.<br /> Now you can {' '}
+                    <Link className ="underline" 
+                        href={'/login'}>Login &raquo;</Link>
+                </div>
+            )}
+            {error && (
+                <div className="my-4 text-center">
+                     An error has occured.<br />
+                    Please try again
+                </div>
+            )}
             <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-                <input type="email" placeholder="email" value={email} 
+                <input type="email" placeholder="email" value={email}
+                    disabled ={creatingUser} 
                     onChange={ev => setEmail(ev.target.value)}></input>
-                <input type="password" placeholder="password" value={password} 
+                <input type="password" placeholder="password" value={password}
+                    disabled ={creatingUser}  
                     onChange={ev => setPassword(ev.target.value)}></input>
-                <button type="submit">Register</button>
+                <button type="submit" 
+                    disabled={creatingUser}
+                    > Register</button>
                 <div className="my-4 text-center text-gray-500">
                     or login with provider </div>
                 <button className="flex gap-4 justify-center">
